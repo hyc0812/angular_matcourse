bd2f29b2a7e5ff26486b5820b5033f93a0795285
yongchang he <hyc0812@163.com>
2022-10-31 16:41:12 -0600

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialDesignModule} from "./material-design/material-design.module";
+import { LoginComponent } from './auth/login/login.component';
+import { TableComponent } from './table/table.component';
+import { GridListComponent } from './grid-list/grid-list.component';
+import { StepperComponent } from './stepper/stepper.component';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TableComponent,
+    GridListComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,12 +17,19 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     MatButtonModule,
     MatCardModule,
-    CommonModule
+    CommonModule,
   ],
   exports:[
     MatButtonModule,
     MatCardModule,
-    
+    MatToolbarModule,
+    MatIconModule,
+    MatTableModule,
+    MatGridListModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class MaterialDesignModule { }
